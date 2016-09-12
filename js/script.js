11034e24cbc2cf76c72fe0dc254dbb98fcd6ae27
lamarn <lev.perschin@gmail.com>
2016-09-12 21:37:15 +0200

    moved links to one js function

--- js/script.js
+++ js/script.js
@@ -20,6 +20,15 @@ function loadPage(site) {
         case 'team':
             window.location = "http://team.lsw.io";
             break;
+        case 'facebook':
+            window.location = "https://www.facebook.com/groups/LinuxstattWindows/";
+            break;
+        case 'twitter':
+            window.location = "https://twitter.com/lsw_io?lang=de";
+            break;
+        case 'youtube':
+            window.location = "https://www.youtube.com/channel/UCfkO3rbaoKNDoB77TZGcQ0Q";
+            break;
         default:
             break;
     }
