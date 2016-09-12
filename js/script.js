2e2489785954354f6571ce96a3793a735ca836ec
lamarn <lev.perschin@gmail.com>
2016-09-12 21:53:46 +0200

    div now clickable on mobile page

--- js/script.js
+++ js/script.js
@@ -9,31 +9,6 @@ function load() {
     startTime();
 }
 
-function loadPage(site) {
-    switch (site){
-        case 'blog':
-            window.location = "http://blog.lsw.io";
-            break;
-        case 'forum':
-            window.location = "http://forum.lsw.io";
-            break;
-        case 'team':
-            window.location = "http://team.lsw.io";
-            break;
-        case 'facebook':
-            window.location = "https://www.facebook.com/groups/LinuxstattWindows/";
-            break;
-        case 'twitter':
-            window.location = "https://twitter.com/lsw_io?lang=de";
-            break;
-        case 'youtube':
-            window.location = "https://www.youtube.com/channel/UCfkO3rbaoKNDoB77TZGcQ0Q";
-            break;
-        default:
-            break;
-    }
-}
-
 function startTime() {
     var days = ['Son', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
